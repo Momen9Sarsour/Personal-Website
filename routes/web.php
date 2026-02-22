e62a48f2a8b127be62619c67181b9253051ab5d0
momen9sarsour <momensarsour5@gmail.com>
2026-02-22 10:55:46 +0200

    Edit web route

--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,11 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
+
 Route::get('/', [FrontController::class, 'index'])->name('front.index');
 Route::get('/resume', [FrontController::class, 'resume'])->name('front.resume');
 Route::get('/projects', [FrontController::class, 'projects'])->name('front.projects');
 Route::get('/contact', [FrontController::class, 'contact'])->name('front.contact');
-
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -20,4 +20,5 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+
+require __DIR__ . '/auth.php';
